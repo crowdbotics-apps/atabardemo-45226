240b5b44955457073735dc28fe12c522c63b2900
Crowdbotics <team@crowdbotics.com>
2023-12-29 19:56:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "atabardemo-45226",
+  url: "https://atabardemo-45226.botics.co",
+  api: "https://atabardemo-45226.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
